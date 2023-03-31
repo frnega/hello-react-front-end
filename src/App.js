71699d80af139cb578fb422cef5d2a8b0acf58ca
Frehiywot Nega <fr.nega@gmail.com>
2023-03-31 13:58:41 +0300

    Fix to eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './Routes';
+import AppRoutes from './routes';
 
 const App = () => (
   <BrowserRouter>
